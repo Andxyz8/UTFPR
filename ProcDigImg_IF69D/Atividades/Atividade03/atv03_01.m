af1032d16365c4fa3b9737206115cd79154da4e0
Andxyz <anderson_x123@hotmail.com>
2024-10-20 11:02:15 -0300

    PDI Atividade 03 Exercício 01

---
+++ ProcDigImg_IF69D/Atividades/Atividade03/atv03_01.m
@@ -0,0 +1,54 @@
+close all; clear all; clc;
+
+function [lut] = get_lut(slope)
+    x = 0:1:255;
+    inflec = 127;
+
+    lut_aux = 1./(1 + exp(-slope*(x - inflec)));
+
+    lut = mat2gray(lut_aux);
+    lut = uint8(lut.*255);
+end
+
+%Sigmoid
+
+% Aloca uint8
+% para depois usar funcao intlut (y1 é a LUT)
+% Equação da sigmoide
+
+slope = 0.05;
+inflec = 127;
+
+x = 0:1:255;
+
+y1 = 1./(1 + exp(-slope*(x - inflec)));
+
+y1n = mat2gray(y1);
+y1n = uint8(y1n.*255);
+
+% Display
+figure, plot(y1n)
+xlim([0 255]), ylim([0 255])
+grid on
+title('Sigmoide')
+xlabel('x'), ylabel('y')
+
+% Leitura da imagem
+A = imread('vpfig.png');
+
+% Img original
+figure, title('Original'), imshow(A);
+
+var = 0.0005;
+
+for n = 1:1:10
+    lut = get_lut(var);
+
+    An = intlut(A, lut);
+
+    % title_text = text(0.5, 0.5, sprintf());
+
+    figure, title('slope %f', n), imshow(An);
+
+    var = 10*var;
+end
