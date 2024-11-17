cb84273c104f581eb73f61bb8c44b4a8fae3d4e4
Andxyz <anderson_x123@hotmail.com>
2024-11-17 17:38:53 -0300

    PDI Atividade 07 Exercício 01 e começo exercício 02

--- ProcDigImg_IF69D/Atividades/Atividade07/atv07_01.m
+++ ProcDigImg_IF69D/Atividades/Atividade07/atv07_01.m
@@ -1,1 +1,38 @@
 close all; clear all; clc;
+
+lista_imagens = {
+    "psl1_gray.png", ...
+    "psl2_gray.png", ...
+    "psl3_gray.png" ...
+};
+
+% Definição do elemento estruturante geral
+se = strel('square', 4);
+
+for i = 1:length(lista_imagens)
+    img = imread(lista_imagens{i});
+
+    figure, subplot(2,2,1), imshow(img)
+    title("Entrada");
+
+    % Realizando abertura
+    a_open = imopen(img, se);
+
+    subplot(2,2,2), imshow(a_open);
+    title("Abertura");
+
+    % Realizando fechamento
+    a_open_closed = imclose(a_open, se);
+
+    subplot(2,2,3), imshow(a_open_closed);
+    title("Abertura + Fechamento");
+
+    % Otsu
+    th = graythresh(a_open_closed);
+
+    % Binarização
+    bw = im2bw(a_open_closed, th);
+
+    subplot(2,2,4), imshow(bw);
+    title("Resultado");
+end
--- ProcDigImg_IF69D/Atividades/Atividade07/atv07_02.m
+++ ProcDigImg_IF69D/Atividades/Atividade07/atv07_02.m
@@ -1,1 +1,35 @@
 close all; clear all; clc;
+
+% IMCOMPLETO: ESSA FOI A IDEIA INICIAL DE COMO
+% ITERAR SOBRE AS IMAGENS, PRECISA FAZER A PARTE
+% DA CATEGORIZAÇÃO DOS OBJETOS CONEXOS
+
+lista_imagens = {
+    "./L/L01.png", ...
+    "./L/L02.png", ...
+    "./L/L03.png", ...
+    "./L/L04.png", ...
+    "./L/L05.png", ...
+    "./L/L06.png", ...
+    "./L/L07.png", ...
+    "./L/L08.png" ...
+};
+
+% Definição do elemento estruturante geral
+se = strel('square', 4);
+
+figure;
+
+for i = 1:length(lista_imagens)
+    img = imread(lista_imagens{i});
+
+    a_open = imopen(img, se);
+    a_open_closed = imclose(a_open, se);
+    th = graythresh(a_open_closed);
+    bw = im2bw(a_open_closed, th);
+
+    subplot(2,4,i);
+
+    title();
+
+end
