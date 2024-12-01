e40abd01a362f09893b24001bc186d3a42b98dbe
Andxyz <anderson_x123@hotmail.com>
2024-12-01 18:05:28 -0300

    PDI Atividade 09 Exercício 02

--- ProcDigImg_IF69D/Atividades/Atividade09/atv09_02.m
+++ ProcDigImg_IF69D/Atividades/Atividade09/atv09_02.m
@@ -1,1 +1,21 @@
 close all; clear all; clc;
+
+img_original = imread("AD21-0016-001_F3_P3_knife_plane_drop_v~small.jpg");
+
+img_d = double(img_original(:, :, 1));
+
+% Normalizar [0, 1]
+img_norm = mat2gray(img_d);
+
+% Inteiros para aplicar colormap
+img_d = uint8(img_norm.*255);
+
+img_pseudocolorida = colormap(jet(256));
+
+subplot(1, 2, 1);
+imshow(img_original);
+title("Imagem original");
+
+subplot(1, 2, 2);
+imshow(img_d, img_pseudocolorida);
+title("Imagem pseudocolorida");
